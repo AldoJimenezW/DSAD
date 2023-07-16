06d502bb8d86a4ba1c6ada044bcb099f1484a553
Bruno Reyes <brreyes@alumnos.uai.cl>
2023-07-16 14:19:06 -0400

    updated code

--- C++/1. Arrays/example1.cpp
+++ C++/1. Arrays/example1.cpp
@@ -5,6 +5,6 @@ int main()
    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
    for (int i = 0; i < 10; i++)
    {
-       std::cout << arr[i] << std::endl;
+       cout << arr[i] << endl;
    }
 }
