a6cd5f88631be86712c888cc2ad2cd42680d555d
Bruno Reyes <brreyes@alumnos.uai.cl>
2023-07-19 18:21:05 -0400

    updated

--- C++/1. Arrays/example1.cpp
+++ C++/1. Arrays/example1.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main()
 {
-   int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-   for (int i = 0; i < 10; i++)
-   {
-       cout << arr[i] << endl;
-   }
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << endl;
+    }
 }
