ef96fff7e04147bd21e5b71c186a65ebc1062561
AldoJimenezW <aldojwchile@gmail.com>
2023-08-02 09:34:26 -0400

    Small fixes

--- C++/1. Arrays/example1.cpp
+++ C++/1. Arrays/example1.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-using namespace std;
-int main()
-{
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    for (int i = 0; i < 10; i++)
-    {
-        cout << arr[i] << endl;
-    }
+int main() {
+  int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Array of 10 integers
+  for (int i = 0; i < 10; i++)                   // Loop to print the array
+  {
+    std::cout << arr[i] << std::endl; // Print the element at index i
+  }
 }
